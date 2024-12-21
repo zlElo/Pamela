1304aa67b56f532873d081158f495d0d043c2be5
zlElo <mail@zlelo.de>
2024-12-21 20:26:35 +0100

    work on receive data and session

--- pamela/app.py
+++ pamela/app.py
@@ -6,7 +6,6 @@ class Pamela:
     
     def create_session(self, address, username=None, password=None):
         self.session.auth = (username, password)
-
         return self.session.post(address)
     
     def close_session(self):
@@ -28,16 +27,17 @@ class Pamela:
 
         # if the session didn't work, use a regular request
         if username and password:
-            response = requests.post(address, auth=(username, password), params=parameters, headers=headers, cookies=cookies, files=files, timeout=timeout, allow_redirects=allow_redirects, proxies=proxies, hooks=hooks, stream=stream, verify=verify, cert=cert)
+            auth = requests.auth.HTTPBasicAuth(username, password)
+            response = requests.get(address, auth=auth, params=parameters, headers=headers, cookies=cookies, files=files, timeout=timeout, allow_redirects=allow_redirects, proxies=proxies, hooks=hooks, stream=stream, verify=verify, cert=cert)
         else:
             response = requests.get(address, params=parameters, headers=headers, cookies=cookies, files=files, timeout=timeout, allow_redirects=allow_redirects, proxies=proxies, hooks=hooks, stream=stream, verify=verify, cert=cert)
             
-            # Check if the response is successful
-            if response.status_code == 200:
-                if json:
-                    data = response.json()
-                else:
-                    data = response.text
-                return data
+        # Check if the response is successful
+        if response.status_code == 200:
+            if json:
+                data = response.json()
             else:
-                return None
+                data = response.text
+            return data
+        else:
+            return None
